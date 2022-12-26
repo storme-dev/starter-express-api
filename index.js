d46923398de8a8bb65a7c9f81faae32f2adffc04
Кирилл Карпицкий <storme-dev@yandex.by>
2022-12-26 19:59:36 +0300

    test

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo test!')
+const https = require('https')
+
+app.get('/dom', (req, res) => {
+
+    let output = '';
+
+    https.get('https://kogda.by/routes/minsk/autobus/37/ДС%20Карбышева%20-%20ДС%20Восточная/Дом%20Печати', (httpResponse) => {
+
+        httpResponse.on('data', (d) => {
+            output += d
+        });
+
+        httpResponse.on('end', () => {
+            res.send(output)
+        })
+
+    })
+
+    // console.log("Just got a request!")
+    // res.send('Yo test!')
 })
 app.listen(process.env.PORT || 3000)
