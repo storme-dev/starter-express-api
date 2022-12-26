4f9d1b04482c20a3eadd782c29db7da779217281
Кирилл Карпицкий <storme-dev@yandex.by>
2022-12-26 20:10:59 +0300

    test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const https = require('https')
 
 app.get('/dom', (req, res) => {
 
+    let result = '';
     let output = '';
 
     https.get('https://kogda.by/routes/minsk/autobus/37/ДС%20Карбышева%20-%20ДС%20Восточная/Дом%20Печати', (httpResponse) => {
@@ -13,10 +14,26 @@ app.get('/dom', (req, res) => {
         });
 
         httpResponse.on('end', () => {
-            res.send(output)
+            const regex = /<span\sclass="future">([^<]*)<\/span>/gm;
+
+            let m;
+
+            while ((m = regex.exec(output)) !== null) {
+                // This is necessary to avoid infinite loops with zero-width matches
+                if (m.index === regex.lastIndex) {
+                    regex.lastIndex++;
+                }
+
+                // The result can be accessed through the `m`-variable.
+                m.forEach((match, groupIndex) => {
+                    result += match.trim().trimEnd() + ' '
+                });
+            }
         })
 
     })
+
+    res.send(result)
 
     // console.log("Just got a request!")
     // res.send('Yo test!')
